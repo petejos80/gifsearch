8df9abebe3c02887f5fa46246fc2e2231088ab99
petejos80 <petejos80@gmail.com>
2018-03-24 17:35:25 -0500

    Added form validation for search query field

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,6 +2,14 @@
 // =============================================================================================
 var subject;
 
+
+
+
+
+
+// FUNCTIONS
+// =============================================================================================
+
 function searchGiphy(searchQuery) {
 // queryURL for Giphy API
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=2UZRAKebFpIBB3Q5RhMHCWc5Clb8vDU4";
@@ -14,15 +22,33 @@ function searchGiphy(searchQuery) {
     });
 };
 
+// If the search query field is empty, alert the user
 function appendSearchQuery() {
-    $("#searchedTerms").append("<button>" + subject + "</button> &nbsp;");
+    if (subject !== "")
+    $("#searchedTerms").append("<button type='button' class='btn btn-light'>" + subject + "</button> &nbsp;");
+};
+
+// Make sure the search query field isn't empty
+function formValidation() {
+    if (subject == "") {
+        alert("Enter a Valid Roll Number");
+        return false;
+    };
 }
- 
+
+
+
+
+
+// GAMEPLAY
+// =============================================================================================
+
 $("#go").on("click", function(event) {
     // Preventing button from submitting form
     event.preventDefault();
     // Storing the artist name
     subject = $("#searchInput").val().trim();
     searchGiphy(subject);
+    formValidation();
     appendSearchQuery();
 });
