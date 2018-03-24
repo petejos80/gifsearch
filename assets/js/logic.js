a780febefeea731d8af1ed900a7df9175e880a7a
petejos80 <petejos80@gmail.com>
2018-03-23 23:29:50 -0500

    Added sidebar

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,17 +1,30 @@
 // GLOBAL VARIABLES
 // =============================================================================================
 
-var searchTerm = "bananas";
 
-
-
+function searchGiphy(searchQuery) {
 // queryURL for Giphy API
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=2UZRAKebFpIBB3Q5RhMHCWc5Clb8vDU4";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=2UZRAKebFpIBB3Q5RhMHCWc5Clb8vDU4";
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-});
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        appendSearchQuery(searchQuery);
+    });
+};
+
+function appendSearchQuery() {
+    $("#searchedTerms").appendTo("o");
+    console.log("bananas");
+}
  
+$("#go").on("click", function(event) {
+    // Preventing button from submitting form
+    event.preventDefault();
+    // Storing the artist name
+    var subject = $("#searchInput").val().trim();
+    searchGiphy(subject);
+    $("#searchedTerms").append("#searchInput");
+});
