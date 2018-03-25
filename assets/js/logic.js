4e6c8b4a772f3aea3521aa520a68c103084d1a10
petejos80 <petejos80@gmail.com>
2018-03-24 19:32:21 -0500

    Appended gifs to elements

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,52 +1,57 @@
 // GLOBAL VARIABLES
 // =============================================================================================
 var subject;
-
-
-
-
-
+var inputText;
 
 // FUNCTIONS
 // =============================================================================================
 
 function searchGiphy(searchQuery) {
-// queryURL for Giphy API
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=2UZRAKebFpIBB3Q5RhMHCWc5Clb8vDU4";
+  // queryURL for Giphy API
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=2UZRAKebFpIBB3Q5RhMHCWc5Clb8vDU4";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
-};
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    var animalDiv = $("#gifArea");
+
+    var animalImage = $("<img>");
+
+    animalImage.attr("src", response.data[0].images.preview_gif.url);
+
+    animalDiv.append(animalImage);
+
+
+  });
+}
 
 // Check that the search query field isn't empty
 function formValidation() {
-    if (subject == "") {
-        alert("Please enter a valid search term");
-        return false;
-    } else {
-        if (subject !== "")
-    $("#searchedTerms").append("<button type='button' class='btn btn-light'>" + subject + "</button> &nbsp;");
+  if (subject == "") {
+    alert("Please enter a valid search term");
+    return false;
+  } else {
+    if (subject !== "")
+      $("#searchedTerms").append("<button type='button' class='btn btn-light'>" + subject + "</button>"
+      );
 
-    // If field is not empty, run the function to perform a Giphy search
+    // If field is not empty, run the function to perform a Giphy search then clear the field.
     searchGiphy(subject);
-    }
-};
-
-
-
-
+    $("form").trigger("reset");
+  }
+}
 
 // GAMEPLAY
 // =============================================================================================
 
 $("#go").on("click", function(event) {
-    // Preventing button from submitting form
-    event.preventDefault();
-    // Storing the artist name
-    subject = $("#searchInput").val().trim();
-    formValidation();
+  // Preventing button from submitting form
+  event.preventDefault();
+  // Storing the artist name
+  subject = $("#searchInput").val().trim();
+  formValidation();
 });
